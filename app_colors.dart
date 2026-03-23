2b0445f8ffaafe82fcf6d253458af48e27986828
kamaya35489 <10965245@students.plymouth.ac.uk>
2026-03-23 10:21:05 +0530

    Add files via upload

---
+++ app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kNavy   = Color(0xFF0B3B66);
+const kSky    = Color(0xFF1E7EC8);
+const kBg     = Color(0xFFF0F6FF);
+const kOrange = Color(0xFFE67E22);
+const kGreen  = Color(0xFF27AE60);
+const kPurple = Color(0xFF8E44AD);
+const kTeal   = Color(0xFF00838F);
